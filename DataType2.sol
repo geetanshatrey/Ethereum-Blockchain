6d3ae0a6a7054f195c8e023e99898c268599a05c
Geetansh Atrey <atrey.geetansh@gmail.com>
2020-02-09 00:03:28 +0530

    Data Types in Ether
    Basic data types in Solidity.

---
+++ DataType2.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.5.11;
+
+contract DataType2 {
+    //1. fixed-size types
+    bool isReady;
+    uint a;
+    address recipient;
+    bytes32 data; //stores binary data
+
+    //2. variable-size types
+    string name;
+    bytes _data;//stores binary data. Different from bytes32 as size is not fixed.
+    uint[] amounts;
+    mapping(uint => string) users;
+
+    //3. user-defined types
+    struct User {
+        uint id;
+        string name;
+        uint[] friendIDs;
+    }
+    enum Color {
+        Red,
+        Green,
+        Blue
+    }
+
+}
